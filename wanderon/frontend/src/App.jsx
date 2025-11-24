74fcd148f3ce0ec48d1cc1fc3ed2125aae48361a
Adarsh6818 <adarshvish01@gmail.com>
2025-11-24 23:00:48 +0000

    Testing code updated in App.jsx to check connection with the Backend.

--- wanderon/frontend/src/App.jsx
+++ wanderon/frontend/src/App.jsx
@@ -1,10 +1,25 @@
-import LandingPage from "./pages/LandingPage"
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState("Message from frontend")
+ 
+  const fetchData = async () => {
+    try {
+    const response  = await fetch("http://localhost:3000/api/hello");
+    const data = await response.json()
 
+    setMessage(data)
+    }
+    catch(err) {
+      setMessage(`Error is: ${err}`)
+    }
+  }
   return (
-      <div className='text-red-500'>
-        <LandingPage />
+      <div>
+        <div>Welcome to Wander On</div>
+        <br/>
+        <button className= "bg-amber-300" onClick={fetchData}> Check Backend</button>
+        <div>Message: {message}</div>
       </div>
   )
 }
