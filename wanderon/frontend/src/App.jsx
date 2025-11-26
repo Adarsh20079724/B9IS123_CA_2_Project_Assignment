9b4039d599bc22b898706c5aab327988e377cf77
Adarsh6818 <adarshvish01@gmail.com>
2025-11-26 14:55:54 +0000

    Testing data fetching from MongoDB.

--- wanderon/frontend/src/App.jsx
+++ wanderon/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 const App = () => {
   const [message, setMessage] = useState("Message from frontend")
  
+  // Fetching Data from Server
   const fetchData = async () => {
     try {
     const response  = await fetch("http://localhost:3000/api/hello");
@@ -14,11 +15,29 @@ const App = () => {
       setMessage(`Error is: ${err}`)
     }
   }
+
+  //Fetching DB Data
+  const fetchDB_Data = async () => {
+    try {
+    const response  = await fetch("http://localhost:3000/api/hello_db");
+    const data = await response.json()
+    console.log("Data fron DB API: ", data)
+    setMessage(data)
+    }
+    catch(err) {
+      setMessage(`Error is: ${err}`)
+    }
+  }
+
+  
+
   return (
       <div>
         <div>Welcome to Wander On</div>
         <br/>
-        <button className= "bg-amber-300" onClick={fetchData}> Check Backend</button>
+        <button className= "bg-amber-300" onClick={fetchData}> Fetch Server Message</button>
+        <br/>
+        <button className= "bg-amber-300" onClick={fetchDB_Data}>Fetch DB Message</button>
         <div>Message: {message}</div>
       </div>
   )
