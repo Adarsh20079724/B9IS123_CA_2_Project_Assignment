fefdcff6a882eaf7ceb45e91e9b6bb98406bdb17
Adarsh6818 <adarshvish01@gmail.com>
2025-11-29 00:12:19 +0000

    Model indexing has been created. (Ref: Best Practices implementation)

---
+++ wanderon/backend/models/modelIndex.js
@@ -0,0 +1,8 @@
+//This is a central export point for all the models in the project
+
+const Message = require("./Message");
+
+module.exports = {
+  Message,
+  // More models will be added here
+};
