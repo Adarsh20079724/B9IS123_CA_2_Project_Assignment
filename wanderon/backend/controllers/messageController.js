bb888da97d1f200edc2589da20e44fe8609f0b30
Adarsh6818 <adarshvish01@gmail.com>
2025-11-29 12:02:41 +0000

    Added message from server as well.

--- wanderon/backend/controllers/messageController.js
+++ wanderon/backend/controllers/messageController.js
@@ -1,5 +1,7 @@
 const Message = require('../models')
 
+// Fetching Test Message from Database 
+
 const getDBMessage = async (req, res) => {
     try {
     // const message = await Message.findOne().sort({ createdAt: -1 });
@@ -24,6 +26,14 @@ const getDBMessage = async (req, res) => {
   }
 }
 
+// Sending back message from server: 
+
+const getServerMessage = async (req, res) => {
+    res.json("This message is from Backend Server")
+}
+
 module.exports = {
     getDBMessage,
+    getServerMessage
 };
+
