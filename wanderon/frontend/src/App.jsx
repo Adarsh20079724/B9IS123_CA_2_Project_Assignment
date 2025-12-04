8d5da6600dd5144708a3f36afda2a4715c6fdba6
Adarsh6818 <adarshvish01@gmail.com>
2025-12-04 20:56:35 +0000

    Testing code is removed and Rounting component has been added for pahge routing tests. (Ref: React Router:  Declarative mode Official website.)

--- wanderon/frontend/src/App.jsx
+++ wanderon/frontend/src/App.jsx
@@ -1,59 +1,10 @@
-import { useState } from "react"
+import RouterConfig from "./router/RouterConfig";
 
 const App = () => {
-  const [message, setMessage] = useState("Message from frontend")
-
-  // Production 
-  //const API_URL = import.meta.env.VITE_BACKEND_API_URL || 'https://wanderon-project-backend.onrender.com'
-
-  //
-  const API_URL = 'http://localhost:3000'
-
-  console.log('Backend API: ',API_URL)
-  // Fetching Data from Server
-  const fetchData = async () => {
-    try {
-    const response = await fetch(`${API_URL}/api/hello`);
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch message');
-      }
-
-    const data = await response.json()
-    console.log("ServerData: ", data);
-    setMessage(data)
-    }
-    catch(err) {
-      setMessage(`Error is: ${err}`)
-    }
-  }
-
-  //Fetching DB Data
-  const fetchDB_Data = async () => {
-    try {
-    const response  = await fetch(`${API_URL}/api/db`);
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch message from DB');
-      }
-
-    const data = await response.json()
-    console.log("Data fron DB API: ", data)
-    setMessage(data.data.message)
-    }
-    catch(err) {
-      setMessage(`Error is: ${err}`)
-    }
-  }
 
   return (
       <div>
-        <div>Welcome to Wander On</div>
-        <br/>
-        <button className= "bg-amber-300" onClick={fetchData}> Fetch Server Message</button>
-        <br/>
-        <button className= "bg-amber-300" onClick={fetchDB_Data}>Fetch DB Message</button>
-        <div>Message: {message}</div>
+        <RouterConfig />
       </div>
   )
 }
