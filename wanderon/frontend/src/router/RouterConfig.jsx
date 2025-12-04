8b651f34579f93ba8c0047eb8491605f337bd7e3
Adarsh6818 <adarshvish01@gmail.com>
2025-12-04 20:56:20 +0000

    Router config file has been created to create routes between the pages(Ref : React router official website)

---
+++ wanderon/frontend/src/router/RouterConfig.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+/**
+ * RouterConfig
+ * - All application routes is defined here.
+ */
+
+const RouterConfig = () => {
+  return (
+    <div>RouterConfig</div>
+  )
+}
+
+export default RouterConfig
