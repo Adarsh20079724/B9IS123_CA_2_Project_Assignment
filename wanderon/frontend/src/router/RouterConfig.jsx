fce5a5ba87a8c1fd6ee0e3b69583206b38b40c03
Adarsh6818 <adarshvish01@gmail.com>
2025-12-04 22:22:48 +0000

    Configure page routes in router config. (ref: Youtube and Official react router docs. Links are present in the comments)

--- wanderon/frontend/src/router/RouterConfig.jsx
+++ wanderon/frontend/src/router/RouterConfig.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
+import LandingPage from "../pages/LandingPage";
+import UserAuthPage from "../pages/UserAuthPage";
+import CreateTripPage from "../pages/CreateTripPage";
+import MyTripsPage from "../pages/MyTripsPage";
 
 /**
  * RouterConfig
  * - All application routes is defined here.
+ * - React-Router Official Docs : https://reactrouter.com/start/data/installation
+ * - Youtube Video Link         : https://youtu.be/-oPldgKUwVA?si=rQj4aigyhvioS7wr
  */
 
 const RouterConfig = () => {
   return (
-    <div>RouterConfig</div>
-  )
-}
+    <Router>
+      <div className="min-h-screen bg-gray-50">
 
-export default RouterConfig
+        {/* Route definitions */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<UserAuthPage />} />
+          <Route path="/create" element={<CreateTripPage />} />
+          <Route path="/create-trip/:id" element={<CreateTripPage />} />
+          <Route path="/my-trips" element={<MyTripsPage />} />
+
+          {/* Catch-all: This will redirect unknown routes to home. By this site won't crash or go into 404 error */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default RouterConfig;
