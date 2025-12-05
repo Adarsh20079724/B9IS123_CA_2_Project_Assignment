d145ad5fc355eca95148d9ed23b9e1d79f725d50
Adarsh6818 <adarshvish01@gmail.com>
2025-12-05 18:07:16 +0000

    Page created to view full itinerary summary.

---
+++ wanderon/frontend/src/pages/ViewItineraryPage.jsx
@@ -0,0 +1,16 @@
+/* ------------------------------------------------------------
+   Page      : ViewItineraryPage.jsx
+   Purpose   : Landing/Home Page for the Website
+   References: Template will be generated soon.
+
+--------------------------------------------------------------*/
+
+import React from 'react'
+
+const ViewItineraryPage = () => {
+  return (
+    <div>ViewItineraryPage</div>
+  )
+}
+
+export default ViewItineraryPage
